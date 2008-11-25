d8883855c660bb7c3587ee854c6f0aa7b975614e
alvakim <alvakim@946fdffe-b6be-11dd-893d-d98698f66bbf>
2008-11-25 22:40:00 +0000

    Результаты тестирования производительности для IE 7.0, Firefox 3.0.4, Google Chrome 0.3.154.9

--- research/performance/sampleText.js
+++ research/performance/sampleText.js
@@ -1,8 +1,17 @@
 var sampleText = [];
+var sampleWords = [];
 
-var template = "0a0b0c0d0e0f0g0h1a1b1c1d1e1f1g1h2a2b2c2d2e2f2g2h3a3b3c3d3e3f3g3h5a5b5c5d5e5f5g5h";
-for(var i=0; i<5000; i++){
+var t1 = "0a0b0c0d0e0f0g0h1a1b1c1d1e";
+var t2 = "1f1g1h2a2b2c2d2e2f2g2h3a3b";
+var t3 = "3c3d3e3f3g3h5a5b5c5d5e5f5g5h";
+
+var template = t1 + t2 + t3;
+for(var i=0; i<1000; i++){
 	sampleText.push(template);
+	
+	sampleWords.push(t1);
+	sampleWords.push(t2);
+	sampleWords.push(t3);
 }
 
 sampleText = sampleText.join("");
